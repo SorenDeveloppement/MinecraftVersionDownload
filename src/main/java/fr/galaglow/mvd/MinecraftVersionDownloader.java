c1ef3439caa264f0877ae68c8e3b6076b16e7ea5
SorenDeveloppement <soren.evrard.lorrain@gmail.com>
2023-08-12 19:30:31 +0200

    saving papers.io download links

--- src/main/java/fr/galaglow/mvd/MinecraftVersionDownloader.java
+++ src/main/java/fr/galaglow/mvd/MinecraftVersionDownloader.java
@@ -2,21 +2,57 @@ package fr.galaglow.mvd;
 
 import fr.galaglow.mvd.utils.Versions;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MinecraftVersionDownloader {
     private Versions mcVersion;
+    private String downloadDirectory;
 
-    public MinecraftVersionDownloader(Versions mcVersion) {
+    public MinecraftVersionDownloader(Versions mcVersion, String downloadDirectory) {
         this.mcVersion = mcVersion;
+        this.downloadDirectory = downloadDirectory;
     }
 
     public String getMcVersionName() {
         return mcVersion.name().toLowerCase();
     }
 
-    public
+    public void downloadVersion() throws IOException {
+        URL version_link = new URL(this.mcVersion.getLink());
+        HttpURLConnection connection = (HttpURLConnection) version_link.openConnection();
+        InputStream in = null;
+        String filename = version_link.getFile();
+        filename = filename.substring(filename.lastIndexOf('/') + 1);
+        FileOutputStream out = new FileOutputStream(this.downloadDirectory + File.separator + filename);
+        in = connection.getInputStream();
+        int read = -1;
+        byte[] buffer = new byte[4096];
+        System.out.println("[DOWNLOAD/INFO]: Downloading file...");
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
+        in.close();
+        out.close();
+        System.out.println("[DOWNLOAD/INFO]: File is correctly downloaded!");
+    }
 
-    public static void main(String[] args) {
-        MinecraftVersionDownloader mvd = new MinecraftVersionDownloader(Versions.Paper1_8_8);
-        System.out.println(mvd.getMcVersionName());
+    private static URL verify(String url){
+        if(!url.toLowerCase().startsWith("http://") || !url.toLowerCase().startsWith("https://")) {
+            return null;
+        }
+        URL verifyUrl = null;
+
+        try{
+            verifyUrl = new URL(url);
+        }catch(Exception e){
+            e.fillInStackTrace();
+        }
+        return verifyUrl;
     }
 }
